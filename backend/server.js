b302df27010107901bf399f9a64f596960700619
Emil Paul <epaul@mta.ca>
2026-04-29 09:34:20 -0300

    fix port

--- backend/server.js
+++ backend/server.js
@@ -139,5 +139,5 @@ app.post('/api/accuse', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
