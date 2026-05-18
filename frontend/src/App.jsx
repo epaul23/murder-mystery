54e3693dac2bb73a837662213926a397c4f89e12
Emil Paul <epaul@mta.ca>
2026-05-18 15:28:12 -0300

    Added analytics vercel

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,17 @@ import { Routes, Route } from 'react-router-dom'
 import CaseSelect from './pages/CaseSelect.jsx'
 import Game from './pages/Game.jsx'
 import Leaderboard from './pages/Leaderboard.jsx'
+import { Analytics } from "@vercel/analytics/react"
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<CaseSelect />} />
-      <Route path="/case/:caseId" element={<Game />} />
-      <Route path="/leaderboard" element={<Leaderboard />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<CaseSelect />} />
+        <Route path="/case/:caseId" element={<Game />} />
+        <Route path="/leaderboard" element={<Leaderboard />} />
+      </Routes>
+      <Analytics />
+    </>
   )
 }
